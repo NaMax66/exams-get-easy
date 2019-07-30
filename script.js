a1b8e81a203d6bff3d9dd17f75f4768693624b39
Максим Найдович <max.naid@mail.ru>
2019-07-30 19:43:41 +0500

    Закрепил меню  сверху, добавил описание серт.

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 (function() {
-    /* "use strict"
-    document.getElementById('header--btn').addEventListener('click', function() {
-        alert('Кнопка нажата! Что теперь?');
-    }) */
+    "use strict";
+
+    let submitFormScript = function() {
+        let sendInfo = function() {
+            console.log('отправляем инфо...');
+
+        }
+        document.getElementById('submit-form').addEventListener('keypress', function(e) {
+            if (e.keyCode === 13 || e.keyCode === 13) {
+                sendInfo();
+            }
+        });
+        document.getElementById('submit-btn').addEventListener('click', sendInfo);
+    }
+
+    document.getElementById('header--btn').addEventListener('click', submitFormScript);
+
+
+
 }())
